4bb53c94f6d1b424ff61e5977eb8e3ee4cd8408d
unknown <xuhang@sz.saregroup.cn>
2016-07-13 11:58:45 +0800

    frist commit src

---
+++ Src/WechatMPCloud/WechatMPCloud/Controllers/TemplateController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WechatMPCloud.Controllers
+{
+    /// <summary>
+    /// 微信公众号菜单页模板
+    /// </summary>
+    public class TemplateController : Controller
+    {
+       
+        /// <summary>
+        /// 活动优惠
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Src/WechatMPCloud/WechatMPCloud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WechatMPCloud.MvcApplication" Language="C#" %>
