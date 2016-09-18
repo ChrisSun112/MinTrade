c01357aee7f427310caebd625cfe43af2fc9337c
徐航 <xuhang1108@126.com>
2016-09-18 16:39:04 +0800

    add 详细页

--- Src/WechatMPCloud/WechatMPCloud/assets/js/goodslist.js
+++ Src/WechatMPCloud/WechatMPCloud/assets/js/goodslist.js
@@ -4,4 +4,11 @@
     $(function () {
       //to do..
     });
+
+    $('.sorting-type>ul>li').click(function () {
+        $(this).parent().children(".am-active").removeClass("am-active");
+        $(this).addClass("am-active");
+     
+    })
+
 })(jQuery);
